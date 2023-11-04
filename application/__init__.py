80a4eb414d17f706ddf1c6d3ec7c6f36d65f9ba6
Michael Mironidis <mam1864@nyu.edu>
2023-11-04 01:15:15 -0400

    cleaning up html version

--- application/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import Config
-
-app = Flask(__name__.split(".", maxsplit=1)[0])
-app.config.from_object(Config)
-
-from application.routes import routes
-app.register_blueprint(routes)
--- application/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-routes = Blueprint("routes","/")
-
-@routes.route("/", methods=["GET"])
-def index():
-    return render_template("index.html")
